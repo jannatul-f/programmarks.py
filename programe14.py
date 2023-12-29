cde726582c36e1228de2280298e6055aef18f5f3
Jannatul Ferdous <137715921+jannatul-f@users.noreply.github.com>
2023-12-29 22:30:37 +0600

    Add files via upload

---
+++ programe14.py
@@ -0,0 +1,21 @@
+#pass/fail
+mark1 = 70
+name1 = "Aysha"
+ID1 = "C2345"
+mark2 = 20
+name2 = "Faria"
+ID2 =  "C2456"
+
+
+if mark1 >= 33:
+  print("Student Name:"+name1)
+  print("ID:"+ID1)
+  print("pass")
+
+
+  print("Student Name:"+name2)
+  print("ID:"+ID2)
+
+  print("pass")
+else:
+  print("fail")
